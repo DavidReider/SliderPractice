8709b2c01d6785e36016d8a4972cfc704704bc9a
DavidReider <freakdj1997@yahoo.com>
2022-04-04 20:54:13 -0400

    Add new slider

--- js/main.js
+++ js/main.js
@@ -1,60 +1,80 @@
-const swiper = new Swiper('.ex1', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    
-    autoplay: {
-        delay: 5000,
-    },
+const swiper = new Swiper(".ex1", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  autoplay: {
+    delay: 5000,
+  },
 
-  });
-
-const swiper2 = new Swiper('.ex2', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    
-    autoplay: {
-        delay: 5000,
-    },
-
-    pagination: {
-        el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-const swiper3 = new Swiper('.ex3', {
+const swiper2 = new Swiper(".ex2", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
+  loop: true,
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper3 = new Swiper(".ex3", {
+  // Optional parameters
+  direction: "horizontal",
   loop: true,
   slidesPerView: 5,
   spaceBetween: 30,
 
   autoplay: {
-      delay: 1750,
+    delay: 3000,
   },
 
   pagination: {
-      el: ".swiper-pagination",
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper4 = new Swiper(".ex4", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 3000,
+    reverseDirection: true,
   },
 
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
